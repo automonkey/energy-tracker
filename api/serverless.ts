742145c38c1f690ea4160ed8ca26e0ce28e5da3f
Will Benyon <git@benyon.io>
2020-08-18 21:08:32 +0100

    Add backend
    Used:
    npx serverless create --template aws-nodejs-typescript

---
+++ api/serverless.ts
@@ -0,0 +1,45 @@
+import { Serverless } from 'serverless/aws';
+
+const serverlessConfiguration: Serverless = {
+  service: {
+    name: 'energy-tracker-api',
+    // app and org for use with dashboard.serverless.com
+    // app: your-app-name,
+    // org: your-org-name,
+  },
+  frameworkVersion: '>=1.72.0',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true
+    }
+  },
+  // Add the serverless-webpack plugin
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    region: 'eu-west-2',
+    runtime: 'nodejs12.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+  },
+  functions: {
+    hello: {
+      handler: 'handler.hello',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: 'hello',
+          }
+        }
+      ]
+    }
+  }
+};
+
+module.exports = serverlessConfiguration;
